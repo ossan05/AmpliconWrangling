5f7ca4de7643dd5d7cfb57dc96a3676cd46a7db9
C0DERP1GLET <75637166+C0DERP1GLET@users.noreply.github.com>
2023-07-31 16:22:46 +0200

    Update sparse_errors.jl

--- Code/General_Alignment/sparse_errors.jl
+++ Code/General_Alignment/sparse_errors.jl
@@ -25,21 +25,20 @@ function initiate_kmerMatching(A::LongDNA{2}, B::LongDNA{2})
     kmerMatching(A, B, match_score, mismatch_score, moveset, affine_score, kmerLength)
 end
 
-function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score::Float64, mismatch_score::Float64, moves::Array{Move}, affine_gap::Float64, kmerLength::Int64 = 12) 
-    return kmerMatching(A, B, make_match_score_matrix(match_score, mismatch_score), moves, affine_gap, kmerLength)
+function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score::Float64, mismatch_score::Float64, reg_moves::Vector{Move}, hgap_moves::Vector{Move}, vgap_moves::Vector{Move}, affine_gap::Float64, kmerLength::Int64 = 12) 
+    return kmerMatching(A, B, make_match_score_matrix(match_score, mismatch_score), reg_moves, hgap_moves, vgap_moves, affine_gap, kmerLength)
 end
-function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score_matrix::Array{Float64, 2}, moves::Array{Move}, affine_gap::Float64, kmerLength::Int64 = 12)
+
+function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score_matrix::Array{Float64, 2}, reg_moves::Vector{Move}, hgap_moves::Vector{Move}, vgap_moves::Vector{Move}, affine_gap::Float64, kmerLength::Int64 = 12)
     k = kmerLength
     kmerDict = Dict{UInt128, Array{Int64}}()
 
-    m = length(A)
-    n = length(B)
+    m, n = length(A), length(B)
     #Initialize List of Kmer Matches
     kmerMatches = KmerMatch[]
     #Initialize Kmer Dictionary from A
-    #k = min(m-2, n-2, floor(Int64, log2(m*n)), 30)
     for i in 1:m-k+1
-        kmer = hash(A[i:i+k-1])
+        kmer = hash(A[i : i + k - 1])
         if kmer in keys(kmerDict)
             push!(kmerDict[kmer], i)
         else
@@ -48,8 +47,8 @@ function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score_matrix::Array{Fl
     end
 
     #Compare B with dictionary
-    diagonals = fill(typemin(Int64), m+n) #diagonals[i] is the kmer in diagonal i furthest to the right. (used to find overlaps in matches)
-    for iB in 1 : n-k+1
+    diagonals = fill(typemin(Int64), m + n) #diagonals[i] is the kmer in diagonal i furthest to the right. (used to find overlaps in matches)
+    for iB in 1 : n - k + 1
         kmer = hash(B[iB : iB+k-1])
         if haskey(kmerDict, kmer)
             for iA in kmerDict[kmer]
@@ -111,23 +110,25 @@ function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score_matrix::Array{Fl
     result = [LongDNA{4}(""), LongDNA{4}("")]
     for kmer in filteredKmerMatches
         if !(kmer.posA == prevA + k && kmer.posB == prevB + k)
-            result .*= general_pairwise_aligner(A[prevA + k : kmer.posA - 1], B[prevB + k : kmer.posB - 1], match_score_matrix, moves, affine_gap)
+            result .*= general_pairwise_aligner(A[prevA + k : kmer.posA - 1], B[prevB + k : kmer.posB - 1], match_score_matrix, reg_moves, hgap_moves, vgap_moves, affine_gap)
         end
         result .*= [A[kmer.posA : kmer.posA + k - 1], B[kmer.posB : kmer.posB + k - 1]]
         prevA = kmer.posA
         prevB = kmer.posB
     end
-    result .*= general_pairwise_aligner(A[prevA + k : m], B[prevB + k : n], match_score_matrix, moves, affine_gap)
+    result .*= general_pairwise_aligner(A[prevA + k : m], B[prevB + k : n], match_score_matrix, reg_moves, hgap_moves, vgap_moves, affine_gap)
     return (result[1], result[2])
 end
 
-# A = LongDNA{2}("ACGGTTAGCGCGCAAGGTCGATGTGTGTGTGTGTG")
-# B = LongDNA{2}("TCGGTTACGCGCAAGGTCGATGAGTGTGTGTG")
+A = LongDNA{2}("ACGGTTAGCGCGCAAGGTCGATGTGTGTGTGTGTG")
+B = LongDNA{2}("TCGGTTACGCGCAAGGTCGATGAGTGTGTGTG")
 # A, B = generate_seq(3000)
 
-# mismatch_score = 0.5
-# match_score = 0.0
-# kmerLength = 30
-# affine_score = 0.5
-# moveset = [Move((1, 1), 0), Move((1, 0), 1), Move((0, 1), 1), Move((3, 3), 0), Move((3, 0), 2), Move((0, 3), 2)]
-# kmerMatching(A, B, match_score, mismatch_score, moveset, affine_score, kmerLength)
+mismatch_score = 1.0
+match_score = 0.0
+kmerLength = 30
+affine_score = 0.5
+reg_moves = [Move(1, 0.5), Move(3, 1)]
+hgap_moves = [Move(3, 4), Move(1, 2)]
+vgap_moves = [Move(3, 4), Move(1, 2)]
+kmerMatching(A, B, match_score, mismatch_score, reg_moves, hgap_moves, vgap_moves, affine_score, kmerLength)
