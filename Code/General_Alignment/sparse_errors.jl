61222e623f039cae09cd7d54774a7499297adcba
ossan05 <oskar.kinell@bahnhof.se>
2023-07-31 20:05:16 +0200

    some changes

--- Code/General_Alignment/sparse_errors.jl
+++ Code/General_Alignment/sparse_errors.jl
@@ -1,6 +1,5 @@
 include("bad_general_pairwise_alignment.jl")
 include("custom_2D_stats.jl")
-export kmerMatching, KmerMatch, Move, general_pairwise_aligner, Gap, make_match_score_matrix
 using BioSequences
 using Kmers
 
@@ -21,12 +20,109 @@ function initiate_kmerMatching(A::LongDNA{2}, B::LongDNA{2})
     match_score = 0.0
     kmerLength = 15
     affine_score = 0.5
-    moveset = [Move((1, 1), 0), Move((1, 0), 1), Move((0, 1), 1), Move((3, 3), 0), Move((3, 0), 2), Move((0, 3), 2)]
-    kmerMatching(A, B, match_score, mismatch_score, moveset, affine_score, kmerLength)
+    gaps = [Move(1, 1.0)]
+    matches = [Move(1, .0)]
+    kmerMatching(A, B, match_score, mismatch_score, matches, gaps, gaps)
 end
 
-function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score::Float64, mismatch_score::Float64, moves::Array{Move}, affine_gap::Float64, kmerLength::Int64 = 12) 
-    return kmerMatching(A, B, make_match_score_matrix(match_score, mismatch_score), moves, affine_gap, kmerLength)
+function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score::Float64, mismatch_score::Float64, matches::Vector{Move}, vgaps::Vector{Move}, hgaps::Vector{Move}, affine_gap::Float64, kmerLength::Int64 = 12) 
+    return kmerMatching(A, B, make_match_score_matrix(match_score, mismatch_score), matches, vgaps, hgaps, affine_gap, kmerLength)
+end
+function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score_matrix::Array{Float64, 2}, moves::Array{Move}, affine_gap::Float64, kmerLength::Int64 = 12)
+    k = kmerLength
+    kmerDict = Dict{UInt128, Array{Int64}}()
+
+    m = length(A)
+    n = length(B)
+    #Initialize List of Kmer Matches
+    kmerMatches = KmerMatch[]
+    #Initialize Kmer Dictionary from A
+    #k = min(m-2, n-2, floor(Int64, log2(m*n)), 30)
+    for i in 1:m-k+1
+        kmer = hash(A[i:i+k-1])
+        if kmer in keys(kmerDict)
+            push!(kmerDict[kmer], i)
+        else
+            kmerDict[kmer] = [i]
+        end
+    end
+
+    #Compare B with dictionary
+    diagonals = fill(typemin(Int64), m+n) #diagonals[i] is the kmer in diagonal i furthest to the right. (used to find overlaps in matches)
+    for iB in 1 : n-k+1
+        kmer = hash(B[iB : iB+k-1])
+        if haskey(kmerDict, kmer)
+            for iA in kmerDict[kmer]
+                if diagonals[iA - iB + n + 1] + k <= iA
+                    push!(kmerMatches, KmerMatch(iA, iB))
+                end
+                diagonals[iA - iB + n + 1] = iA
+            end
+        end
+    end
+
+    #Prune set of kmers
+    matchCount = length(kmerMatches)
+    deletionFlags = BitArray([0 for i in 1:matchCount])
+    kmerMetrics = GetSampleMetrics2D(map(x -> x.posA, kmerMatches), map(x -> x.posB, kmerMatches))
+    corScore = CorrelationScore(kmerMetrics)
+    for i in 1 : matchCount
+        bestDeletion = -1
+        if kmerMetrics.n <= 2
+            break
+        end
+        for j in 1 : matchCount
+            if !deletionFlags[j]
+                kmer = kmerMatches[j]
+                newCorScore = CorrelationScore(RemoveVector(kmerMetrics, kmer.posA, kmer.posB))
+                if newCorScore > corScore
+                    corScore = newCorScore
+                    bestDeletion = j
+                end
+            end
+        end
+        if bestDeletion == -1
+            break
+        else
+            deletionFlags[bestDeletion] = true
+            kmer = kmerMatches[bestDeletion]
+            kmerMetrics = RemoveVector(kmerMetrics, kmer.posA, kmer.posB)
+            corScore = CorrelationScore(kmerMetrics)
+        end
+    end
+
+    #Ensure that kmers are compatible
+    filteredKmerMatches = KmerMatch[]
+    prevA = -k
+    prevB = -k
+    for i in 1 : matchCount
+        match = kmerMatches[i]
+        if !deletionFlags[i] && prevA + k <= match.posA && prevB + k <= match.posB
+            prevA = match.posA
+            prevB = match.posB
+            push!(filteredKmerMatches, match)
+        end
+    end
+
+    prevA = -k+1
+    prevB = -k+1
+    #Join kmers using needleman Wunsch
+    #To first Kmer
+    result = [LongDNA{4}(""), LongDNA{4}("")]
+    for kmer in filteredKmerMatches
+        if !(kmer.posA == prevA + k && kmer.posB == prevB + k)
+            result .*= general_pairwise_aligner(A[prevA + k : kmer.posA - 1], B[prevB + k : kmer.posB - 1], match_score_matrix, moves, affine_gap)
+        end
+        result .*= [A[kmer.posA : kmer.posA + k - 1], B[kmer.posB : kmer.posB + k - 1]]
+        prevA = kmer.posA
+        prevB = kmer.posB
+    end
+    result .*= general_pairwise_aligner(A[prevA + k : m], B[prevB + k : n], match_score_matrix, moves, affine_gap)
+    return (result[1], result[2])
+end
+
+function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score::Float64, mismatch_score::Float64, matches::Vector{Move}, vgaps::Vector{Move}, hgaps::Vector{Move}, kmerLength::Int64 = 12) 
+    return kmerMatching(A, B, make_match_score_matrix(match_score, mismatch_score), moves, kmerLength)
 end
 function kmerMatching(A::LongDNA{2}, B::LongDNA{2}, match_score_matrix::Array{Float64, 2}, moves::Array{Move}, affine_gap::Float64, kmerLength::Int64 = 12)
     k = kmerLength
